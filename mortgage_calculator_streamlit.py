5efc3add47b8473da1779cf9bd919ad3891a7944
Andreas Theodoulou <54478025+AndreasTheodoulou@users.noreply.github.com>
2025-02-19 23:04:14 +0000

    Update mortgage_calculator_streamlit.py

--- mortgage_calculator_streamlit.py
+++ mortgage_calculator_streamlit.py
@@ -79,7 +79,7 @@ def main():
         # Equity lookup
         st.subheader("Retrieve Equity at Specific Time")
         year_equity = st.number_input("Year", min_value=0, value=5)
-        month_equity = st.number_input("Month", min_value=1, max_value=12, value=6)
+        month_equity = st.number_input("Month", min_value=1, max_value=12, value=1)
 
         if st.button("Get Equity"):
             equity = get_equity_at_year_month(st.session_state.df, year_equity, month_equity)
